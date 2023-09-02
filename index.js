41b787381de5a8da9d46a147dd14cfc8b55e92ad
SidneyKing <sid8121@gmail.com>
2023-09-02 16:49:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('The DOM has loaded')
+    document.querySelector('p')
+const paragraph = document.getElementById('text')
+paragraph.textContent = 'This is really cool!'
+})
+
